d1f67d08678f9cfce709df0fe245b68ecc6c7f0e
h1lsx <139397050+h1lsx@users.noreply.github.com>
2023-09-26 14:56:12 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,24 @@
-
-self.addEventListener (‘fetch’, function(event) {
+const cacheName = "v1";
+const cacheURLs = [
+    "/",
+    "/build/nds.js",
+    "/build/nds.wasm",
+    "/app.wasm",
+    "/build/",
+    "/audio-worklet.js",
+    "/dark.css",
+    "/icon.png",
+    "/index.html",
+    "/localforage.js",
+    "/pako.min.js",
+    "/style.css",
+    "/nsmb.nds",
+    "/sw.js"
+];
+var abc = document.getElementById("install-button");
+abc.addEventListener('click', a => {
+    caches.open(cacheName).then(cache.addAll(cacheURLs))
+})
+self.addEventListener ('fetch', function(event) {
     
 });
